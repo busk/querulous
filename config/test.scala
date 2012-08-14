b5f528e69e49ffb554e4bf503b035fe64c5b499e
Nermin <nserifovic@bostontechnologies.com>
2012-08-14 14:37:07 -0400

    Migrated to SBT 0.11.0 and Scala 2.9.1

--- config/test.scala
+++ config/test.scala
@@ -3,13 +3,6 @@ import com.twitter.querulous.config.Connection
 new Connection {
   val hostnames = Seq("localhost")
   val database = "db_test"
-  val username = {
-    val userEnv = System.getenv("DB_USERNAME")
-    if (userEnv == null) "root" else userEnv
-  }
-
-  val password = {
-    val passEnv = System.getenv("DB_PASSWORD")
-    if (passEnv == null) "" else passEnv
-  }
+  val username = System.getenv("DB_USERNAME")   
+  val password = System.getenv("DB_PASSWORD")
 }
