be5fd6fd8585f7cce806c4af6b1856bd54b0dbe8
Noah Cornwell <ncornwell@bostontechnologies.com>
2013-02-25 10:04:50 -0500

    Upgrading to scala 2.10

--- project/Build.scala
+++ project/Build.scala
@@ -5,8 +5,8 @@ object QuerulousProject extends Build {
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.bostontechnologies",
-    scalaVersion := "2.9.1",   
-    version := "1.0.4",
+    scalaVersion := "2.10.0",   
+    version := "1.0.5",
     publishTo := Some("BT maven artifactory" at "http://maven.bostontechnologies.com/artifactory/libs-releases-local"),
     credentials += Credentials("Artifactory Realm", "maven.bostontechnologies.com", "artifactory", "Bt2@rTif@ct0ry!")
   )
@@ -14,10 +14,10 @@ object QuerulousProject extends Build {
   val dbcp      = "commons-dbcp" % "commons-dbcp"         % "1.4"
   val mysqljdbc = "mysql"        % "mysql-connector-java" % "5.1.18"
   val pool      = "commons-pool" % "commons-pool"         % "1.5.4"
-  val utilCore  = "com.twitter" %% "util-core"            % "4.0.1"
+  val utilCore  = "com.twitter" %% "util-core"            % "6.1.0"
   val scalaz    = "org.scalaz"  %% "scalaz-core"          % "6.0.4"
   
-  val utilEval   = "com.twitter"            %% "util-eval"          % "4.0.1"	  % "test"  
+  val utilEval   = "com.twitter"            %% "util-eval"          % "6.1.0"	  % "test"  
   val hamcrest   = "org.hamcrest"            % "hamcrest-all"       % "1.1"       % "test"
   val specs      = "org.scala-tools.testing" % "specs_2.8.0"        % "1.6.5"     % "test"
   val objenesis  = "org.objenesis"           % "objenesis"          % "1.1"       % "test"
@@ -27,11 +27,13 @@ object QuerulousProject extends Build {
   val dbcpTests  = "commons-dbcp"            % "commons-dbcp-tests" % "1.4"       % "test"
 
   val dependencies = Seq(dbcp, mysqljdbc, pool, utilCore, utilEval, hamcrest, specs, objenesis, jmock,
-	cglib, asm, dbcpTests, scalaz)
+	cglib, asm, scalaz)
 
   val twitterRepo = "Twitter Maven Repo" at "http://maven.twttr.com/"
 
-  val customResolvers = Seq(twitterRepo)
+  val typesafeRepo = "Typesafe repo" at "http://repo.typesafe.com/typesafe/releases/"
+
+  val customResolvers = Seq(twitterRepo, typesafeRepo)
 
   val root = Project(
     "querulous", 
